8dbe11275d8747ce0892b55c49b0e2858f3dc6e4
Kaiser Li <kaiserli@gmail.com>
2022-11-02 12:16:59 +0800

    update: 更新kuyiToneReadme

--- kuyiTone/kuyiTone.py
+++ kuyiTone/kuyiTone.py
@@ -3,37 +3,42 @@ import winsound
 import yaml
 import codecs
 import sys
+import qrcode
+import base64
+import struct
+
 
 def parseTone(tone: str):
-    baseTone = 25 # C调的do的index
+    baseTone = 25  # C调的do的index
     tone = tone.strip()
 
     match tone[0]:
         case 'C':
-            pass            
+            pass
         case 'D':
             baseTone += 2
-        case 'E':           
+        case 'E':
             baseTone += 4
-        case 'F': 
+        case 'F':
             baseTone += 5
-        case 'G':           
+        case 'G':
             baseTone += 7
-        case 'A':           
+        case 'A':
             baseTone += 9
-        case 'B':  
+        case 'B':
             baseTone += 11
 
-    if len(tone)>1:
+    if len(tone) > 1:
         match tone[1]:
             case '#':
                 baseTone += 1
             case 'b':
-                baseTone -= 1    
+                baseTone -= 1
 
     return baseTone
 
-def parseBeat(beat:str, baseTone:int, bpm:int):
+
+def parseBeat(beat: str, baseTone: int, bpm: int):
     beat = beat.strip()
     desc = beat[1:]
     tone = baseTone
@@ -56,8 +61,8 @@ def parseBeat(beat:str, baseTone:int, bpm:int):
                 tone -= 1
             case ':':
                 b += b / 2
-    
-    t = int(b * 1000 * 60 / bpm )
+
+    t = int(b * 1000 * 60 / bpm)
     p = 0
 
     match beat[0]:
@@ -74,22 +79,25 @@ def parseBeat(beat:str, baseTone:int, bpm:int):
         case '5':
             p = tone + 7
         case '6':
-            p = tone + 9            
+            p = tone + 9
         case '7':
-            p = tone + 11     
+            p = tone + 11
 
-    #http://www.360doc.com/content/22/0110/21/111224_1012721857.shtml
-    tones = (0, 
-            65, 69, 73, 77, 82, 87, 92, 98, 103, 110, 116, 123, # 0
-            130, 138, 146, 155, 164, 174, 185, 196, 207, 220, 233, 246, # 1
-            261, 277, 293, 311, 326, 349, 370, 392, 415, 440, 466, 493, # 2
-            523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 987, # 3
-            1046, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976 # 4
-            )           
+    # http://www.360doc.com/content/22/0110/21/111224_1012721857.shtml
+    tones = (
+        0,
+        65, 69, 73, 77, 82, 87, 92, 98, 103, 110, 116, 123,  # 0
+        130, 138, 146, 155, 164, 174, 185, 196, 207, 220, 233, 246,  # 1
+        261, 277, 293, 311, 326, 349, 370, 392, 415, 440, 466, 493,  # 2
+        523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 987,  # 3
+        1046, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976  # 4
+    )
 
     return (tones[p], t)
 
+
 def parseMusic(m):
+    print("data:")
     print(m)
     baseTone = parseTone(m['tone'])
     bpm = m['bpm']
@@ -97,17 +105,34 @@ def parseMusic(m):
     beats = (parseBeat(x, baseTone, bpm) for x in m['music'].split())
     return beats
 
+
 def playBeats(beats):
     for b in beats:
         print(b)
-        if(b[0] == 0):
+        if (b[0] == 0):
             sleep(b[1] // 1000)
         else:
-            winsound.Beep(b[0], b[1])  
+            winsound.Beep(b[0], b[1])
+
 
 def printBeats(beats):
-    out = [hex(o) for b in beats for o in b]   
-    print(f'[{",".join(out)}]')
+    out = [o for b in beats for o in b]
+    print("raw_16bit:")
+    print(f'[{",".join([hex(o) for o in out])}]')
+
+    bs = struct.pack('H'*len(out), *out)
+
+    print("raw_8bit:")
+    print(f'[{",".join([hex(o) for o in bs])}]')
+
+    b64 = base64.b64encode(bs)
+
+    print("base64:")
+    print(b64)
+    qr = qrcode.QRCode()
+    qr.add_data(b64)
+    qr.print_ascii()
+
 
 def readMusicFile(fname):
     with codecs.open(fname, 'r', 'utf8') as f:
@@ -115,8 +140,10 @@ def readMusicFile(fname):
 
     return m
 
+
 def printUsage():
     print("kuyiTone [play|gen] <filename>")
+
 
 def main():
     if len(sys.argv) != 3:
@@ -127,7 +154,7 @@ def main():
     cmd = sys.argv[1]
     try:
         m = readMusicFile(fname)
-        beats = parseMusic(m) 
+        beats = parseMusic(m)
         match cmd.lower():
             case 'play':
                 playBeats(beats)
@@ -137,6 +164,7 @@ def main():
     except Exception as e:
         print(e)
         return
+
 
 if __name__ == '__main__':
     main()
